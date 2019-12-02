4c9788d64e974a224cc3f203b245cbdfa648cd4d
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-02 17:28:54 +0300

    update generate menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,19 @@
-export const createMenuTemplate = () => {
+const createMenuMarkup = (item, isChecked, isLast) => {
+  const {name, code, count} = item;
+
+  return (
+    `<a href="#${code}"
+        class="main-navigation__item ${isChecked ? `main-navigation__item--active` : ``} ${isLast ? ` main-navigation__item--additional` : ``}">
+        ${name} ${count ? `<span class="main-navigation__item-count">${count}</span>` : ``}</a>`
+  );
+};
+
+export const createMenuTemplate = (menuItems) => {
+  const menuMarkup = menuItems.map((item, i) => createMenuMarkup(item, i === 0, i === menuItems.length - 1)).join(`\n`);
+
   return (
     `<nav class="main-navigation">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-      <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+      ${menuMarkup}
     </nav>`
   );
 };
