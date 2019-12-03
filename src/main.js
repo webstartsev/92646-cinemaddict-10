742097c54422a1b4474d2056ebd5605391d7052e
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-03 17:44:53 +0300

    import all components

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import {createProfileTemplate} from './components/profile';
-import {createMenuTemplate} from './components/menu';
-import {createSortTemplate} from './components/sort';
-import {createFilmsTemplate} from './components/films';
-import {createFilmTemplate} from './components/film';
-import {createShowMoreBtnTemplate} from './components/show-more-btn';
-import {createFilmsTopTemplate} from './components/films-top';
-import {createFilmsMostTemplate} from './components/films-most';
-import {createFilmPopupTemplate} from './components/film-popup';
-import {createCommentsTemplate} from './components/comments';
+import ProfileComponent from './components/profile';
+import MenuComponent from './components/menu';
+import SortComponent from './components/sort';
+import FilmsComponent from './components/films';
+import FilmComponent from './components/film';
+import ShowMoreBtnComponent from './components/show-more-btn';
+import FilmsTopComponent from './components/films-top';
+import FilmsMostComponent from './components/films-most';
+import FilmPopupComponent from './components/film-popup';
+import CommentsComponent from './components/comments';
 import {generateFilms} from './mock/film.js';
 import {generateMenu} from './mock/menu.js';
 import {generateComments} from './mock/comment.js';
@@ -22,44 +22,44 @@ const films = generateFilms(FILM_COUNT);
 const menuItems = generateMenu(films);
 
 const headerElement = document.querySelector(`.header`);
-render(headerElement, createProfileTemplate());
+render(headerElement, new ProfileComponent().getElement());
 
 const mainElement = document.querySelector(`.main`);
-render(mainElement, createMenuTemplate(menuItems));
-render(mainElement, createSortTemplate());
-render(mainElement, createFilmsTemplate());
+render(mainElement, new MenuComponent(menuItems).getElement());
+render(mainElement, new SortComponent().getElement());
+render(mainElement, new FilmsComponent().getElement());
 
 const filmsElement = mainElement.querySelector(`.films`);
 const filmListContainerElement = filmsElement.querySelector(`.films-list__container`);
 
 let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
 films.slice(0, showingFilmsCount)
-  .forEach((film) => render(filmListContainerElement, createFilmTemplate(film)));
+  .forEach((film) => render(filmListContainerElement, new FilmComponent(film).getElement()));
 
 const filmsListElement = mainElement.querySelector(`.films-list`);
-render(filmsListElement, createShowMoreBtnTemplate());
+render(filmsListElement, new ShowMoreBtnComponent().getElement());
 
 const filmTopList = films.slice().sort((a, b) => b.rating - a.rating).slice(0, FILM_EXTRA_COUNT);
 let filterFilm = filmTopList.filter((film) => film.rating > 0);
 if (filterFilm.length) {
-  render(filmsElement, createFilmsTopTemplate());
+  render(filmsElement, new FilmsTopComponent().getElement());
   const filmsListTopElement = mainElement.querySelector(`.films-list--extra-top`);
   const filmsListTopContainerElement = filmsListTopElement.querySelector(`.films-list__container`);
-  filmTopList.forEach((film) => render(filmsListTopContainerElement, createFilmTemplate(film)));
+  filmTopList.forEach((film) => render(filmsListTopContainerElement, new FilmComponent(film).getElement()));
 }
 
 const filmMostComment = films.slice().sort((a, b) => b.comments - a.comments).slice(0, FILM_EXTRA_COUNT);
 filterFilm = filmTopList.filter((film) => film.comments > 0);
 if (filterFilm.length) {
-  render(filmsElement, createFilmsMostTemplate());
+  render(filmsElement, new FilmsMostComponent().getElement());
   const filmsListMostCommentElement = mainElement.querySelector(`.films-list--extra-most`);
   const filmsListMostCommentContainerElement = filmsListMostCommentElement.querySelector(`.films-list__container`);
-  filmMostComment.forEach((film) => render(filmsListMostCommentContainerElement, createFilmTemplate(film)));
+  filmMostComment.forEach((film) => render(filmsListMostCommentContainerElement, new FilmComponent(film).getElement()));
 }
-render(document.querySelector(`body`), createFilmPopupTemplate(films[0]));
+render(document.querySelector(`body`), new FilmPopupComponent(films[0]).getElement());
 const popupBottomElement = document.querySelector(`.form-details__bottom-container`);
 const comments = generateComments(films[0].comments);
-render(popupBottomElement, createCommentsTemplate(comments));
+render(popupBottomElement, new CommentsComponent(comments).getElement());
 
 const loadMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
 loadMoreButton.addEventListener(`click`, () => {
@@ -67,7 +67,7 @@ loadMoreButton.addEventListener(`click`, () => {
   showingFilmsCount = showingFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON;
 
   films.slice(prevTasksCount, showingFilmsCount)
-    .forEach((film) => render(filmListContainerElement, createFilmTemplate(film)));
+    .forEach((film) => render(filmListContainerElement, new FilmComponent(film).getElement()));
 
   if (showingFilmsCount >= films.length) {
     loadMoreButton.remove();
