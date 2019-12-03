ff8c89ca4f99c61e6fb96f802c7fa2aebc821d78
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-03 17:38:02 +0300

    update ShowMoreBtn to class

--- src/components/show-more-btn.js
+++ src/components/show-more-btn.js
@@ -1,5 +1,29 @@
-export const createShowMoreBtnTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createShowMoreBtnTemplate = () => {
   return (
     `<button class="films-list__show-more">Show more</button>`
   );
 };
+
+export default class ShowMoreBtn {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createShowMoreBtnTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
