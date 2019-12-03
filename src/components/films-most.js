73fd85de05103e4179b0af5699fa6b640b3fbfe0
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-03 17:40:31 +0300

    update FilmsMost to class

--- src/components/films-most.js
+++ src/components/films-most.js
@@ -1,4 +1,6 @@
-export const createFilmsMostTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createFilmsMostTemplate = () => {
   return (
     `<section class="films-list--extra films-list--extra-most">
       <h2 class="films-list__title">Most commented</h2>
@@ -7,3 +9,26 @@ export const createFilmsMostTemplate = () => {
     </section>`
   );
 };
+
+export default class FilmsMost {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmsMostTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
