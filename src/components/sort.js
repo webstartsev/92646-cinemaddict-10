4b847d17852bd77e4d32e35c114f26c1787bf3ca
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-09 17:38:51 +0300

    update sort component and add setClickSortHandler

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,17 +1,61 @@
 import AbstractComponent from './abstract-component.js';
 
-const createSortTemplate = () => {
+const sortItems = [
+  {
+    title: `Sort by default`,
+    type: `default`,
+  },
+  {
+    title: `Sort by date`,
+    type: `date`,
+  },
+  {
+    title: `Sort by rating`,
+    type: `rating`,
+  }
+];
+
+const createSortMarkup = (item, currentSortType) => {
+  const activeClass = (currentSortType === item.type) ? `sort__button--active` : ``;
+
+  return (
+    `<li><a href="#" class="sort__button ${activeClass}" data-sort-type="${item.type}">${item.title}</a></li>`
+  );
+};
+
+const createSortTemplate = (currentSortType) => {
+  const sortMarkup = sortItems.map((item) => createSortMarkup(item, currentSortType)).join(`\n`);
+
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      ${sortMarkup}
     </ul>`
   );
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = `default`;
+  }
+
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._currentSortType);
+  }
+
+  setClickSortHandler(handler) {
+    const sortElements = this.getElement().querySelectorAll(`.sort__button`);
+    sortElements.forEach((element) => element.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      const sortType = evt.target.dataset.sortType;
+      if (sortType === this._currentSortType) {
+        return;
+      }
+
+      this._currentSortType = sortType;
+      handler(sortType);
+    }));
   }
 }
