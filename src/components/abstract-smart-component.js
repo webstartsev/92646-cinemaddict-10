43ad78c4f113009999eb5fe0a368fd061d67a5cf
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-17 15:34:22 +0300

    add AbstractSmartComponent

---
+++ src/components/abstract-smart-component.js
@@ -0,0 +1,20 @@
+import AbstarctComponent from './abstract-component.js';
+
+export default class AbstractSmartComponent extends AbstarctComponent {
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
+  }
+
+  rerender() {
+    const oldElement = this.getElement();
+    const parent = oldElement.parentElement;
+
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, oldElement);
+
+    this.recoveryListeners();
+  }
+}
