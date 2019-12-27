a9ae2444fc5d3fe2285deb9729f9d87d6f0cddc3
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-27 16:29:39 +0300

    update StatisticsRank component

--- src/components/statistics-rank.js
+++ src/components/statistics-rank.js
@@ -1,18 +1,23 @@
 
 import AbstractComponent from "./abstract-component.js";
 
-const createStatisticsRankTemplate = () => {
+const createStatisticsRankTemplate = (rank) => {
   return (
     `<p class="statistic__rank">
       Your rank
       <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-      <span class="statistic__rank-label">Sci-Fighter</span>
+      <span class="statistic__rank-label">${rank}</span>
     </p>`
   );
 };
 
 export default class StatisticsRank extends AbstractComponent {
+  constructor(rank) {
+    super();
+
+    this._rank = rank;
+  }
   getTemplate() {
-    return createStatisticsRankTemplate();
+    return createStatisticsRankTemplate(this._rank);
   }
 }
