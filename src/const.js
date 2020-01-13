3a820dd18a13f9e22300db32ebeff42dbdd5534b
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-13 15:33:28 +0300

    add const for API

--- src/const.js
+++ src/const.js
@@ -1,3 +1,6 @@
+export const AUTHORIZATION = `Basic eo0w590ik29889a`;
+export const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict/`;
+
 export const SortType = {
   DEFAULT: `default`,
   DATE: `date`,
