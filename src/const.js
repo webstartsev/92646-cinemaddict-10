196f83213ae584043187703b13795e1ab4464f05
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-14 14:46:25 +0300

    update END_POINT path

--- src/const.js
+++ src/const.js
@@ -1,5 +1,5 @@
 export const AUTHORIZATION = `Basic eo0w590ik29889a`;
-export const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict/`;
+export const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict`;
 
 export const SortType = {
   DEFAULT: `default`,
