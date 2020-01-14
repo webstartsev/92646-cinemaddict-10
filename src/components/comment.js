c8823d010e76d9b308b9e1febce6839e70e13fc2
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-14 10:30:29 +0300

    update fields from server

--- src/components/comment.js
+++ src/components/comment.js
@@ -2,18 +2,18 @@ import AbstractComponent from './abstract-component.js';
 import {formatDate} from "../utils/comment.js";
 
 const createCommentsTemplate = (commet) => {
-  const {text, user, emoji} = commet;
+  const {comment, author, emotion} = commet;
   const date = formatDate(commet.date);
 
   return (
     `<li class="film-details__comment">
     <span class="film-details__comment-emoji">
-      <img src="./images/emoji/${emoji}" width="55" height="55" alt="emoji">
+      <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji">
     </span>
     <div>
-      <p class="film-details__comment-text">${text}</p>
+      <p class="film-details__comment-text">${comment}</p>
       <p class="film-details__comment-info">
-        <span class="film-details__comment-author">${user}</span>
+        <span class="film-details__comment-author">${author}</span>
         <span class="film-details__comment-day">${date}</span>
         <button class="film-details__comment-delete">Delete</button>
       </p>
