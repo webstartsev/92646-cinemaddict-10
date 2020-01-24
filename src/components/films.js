adb66e4b96baca4b264cc1ceb7eeb6e89026359b
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-24 11:21:21 +0300

    add setTitle & setDefaultTitle method

--- src/components/films.js
+++ src/components/films.js
@@ -1,10 +1,13 @@
 import AbstractComponent from './abstract-component.js';
+import {HIDDEN_CLASS} from '../const.js';
 
-const createFilmsTemplate = () => {
+const DEFAULT_TITLE = `All movies. Upcoming`;
+
+const createFilmsTemplate = (title) => {
   return (
     `<section class="films">
       <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+        <h2 class="films-list__title visually-hidden">${title}</h2>
 
         <div class="films-list__container"></div>
       </section>
@@ -13,7 +16,25 @@ const createFilmsTemplate = () => {
 };
 
 export default class Films extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._title = DEFAULT_TITLE;
+  }
   getTemplate() {
-    return createFilmsTemplate();
+    return createFilmsTemplate(this._title);
+  }
+
+  setTitle(title) {
+    this._title = title;
+    const titleElement = this.getElement().querySelector(`.films-list__title`);
+    titleElement.innerHTML = this._title;
+    titleElement.classList.remove(HIDDEN_CLASS);
+  }
+
+  setDefaultTitle() {
+    const titleElement = this.getElement().querySelector(`.films-list__title`);
+    titleElement.innerText = DEFAULT_TITLE;
+    titleElement.classList.add(HIDDEN_CLASS);
   }
 }
