82bd18c435b7207775230ea1ff8fe9f5213d50dc
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-27 13:57:43 +0300

    add emoji puke

--- src/components/comments.js
+++ src/components/comments.js
@@ -51,6 +51,11 @@ const createCommentsTemplate = () => {
           <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
         </label>
 
+        <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
+        <label class="film-details__emoji-label" for="emoji-puke">
+          <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
+        </label>
+
         <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
         <label class="film-details__emoji-label" for="emoji-angry">
           <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
