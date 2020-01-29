effba47834d56c98654c420ffd20fc539f8ef875
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-29 15:54:58 +0300

    updare prepearMovies

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,5 +1,3 @@
-import {getWatchlistMovies, getHistoryMovies, getFavoriteMovies} from './movie.js';
-
 const menuItems = [
   {name: `All movies`, code: `all`},
   {name: `Watchlist`, code: `watchlist`},
@@ -9,22 +7,30 @@ const menuItems = [
 ];
 
 const prepearMovies = (movies) => {
-  return {
-    Watchlist: getWatchlistMovies(movies).length,
-    History: getHistoryMovies(movies).length,
-    Favorites: getFavoriteMovies(movies).length,
-  };
+  return movies.reduce((acc, movie) => {
+    if (movie.isNeedWatch) {
+      acc[`watchlist`] = acc.hasOwnProperty(`watchlist`) ? ++acc[`watchlist`] : 1;
+    }
+
+    if (movie.isWatch) {
+      acc[`history`] = acc.hasOwnProperty(`history`) ? ++acc[`history`] : 1;
+    }
+
+    if (movie.isFavorite) {
+      acc[`favorites`] = acc.hasOwnProperty(`favorites`) ? ++acc[`favorites`] : 1;
+    }
+
+    return Object.assign({}, acc);
+  }, {});
 };
 
-const generateMenu = (movies) => {
+export const generateMenu = (movies) => {
   const moviesByFilter = prepearMovies(movies);
   return menuItems.map((item) => {
     return {
       name: item.name,
       code: item.code,
-      count: moviesByFilter[item.name],
+      count: moviesByFilter[item.code],
     };
   });
 };
-
-export {generateMenu};
