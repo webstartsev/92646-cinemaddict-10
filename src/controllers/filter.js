6635230fab3ecc716abe793d499de693ea4761d9
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-01-29 15:56:10 +0300

    refactor

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -1,18 +1,15 @@
 import MenuComponent from '../components/menu.js';
 import {generateMenu} from '../utils/menu.js';
 import {render, replace} from '../utils/render.js';
-import {FilterType} from "../const.js";
 
 export default class FilterController {
   constructor(container, movieModel) {
     this._container = container;
     this._movieModel = movieModel;
-    this._activeFilterType = FilterType.ALL;
 
     this._menuComponent = null;
     this._setOnChangeMenuHandler = null;
 
-    this._onFilterChange = this._onFilterChange.bind(this);
     this._onDataChange = this._onDataChange.bind(this);
 
     this._movieModel.setDataChangeHandler(this._onDataChange);
@@ -24,7 +21,7 @@ export default class FilterController {
     const films = this._movieModel.getMovies();
     const menuItems = generateMenu(films);
     this._menuComponent = new MenuComponent(menuItems);
-    this._recoveryListeners();
+    this.setOnChange(this._setOnChangeMenuHandler);
 
     if (oldComponent) {
       replace(this._menuComponent, oldComponent);
@@ -33,27 +30,18 @@ export default class FilterController {
     }
   }
 
+  _onDataChange() {
+    this.render();
+  }
+
   setOnChange(handler) {
     this._setOnChangeMenuHandler = handler;
     this._menuComponent.setOnChange((menuItem) => {
+      this._movieModel.setFilter(menuItem);
       this._menuComponent.setActiveMenu(menuItem);
       if (this._setOnChangeMenuHandler) {
         this._setOnChangeMenuHandler(menuItem);
       }
     });
-  }
-
-  _onFilterChange(filterName) {
-    this._movieModel.setFilter(filterName);
-    this._activeFilterType = filterName;
-  }
-
-  _onDataChange() {
-    this.render();
-  }
-
-  _recoveryListeners() {
-    this._menuComponent.setOnChange(this._onFilterChange);
-    this.setOnChange(this._setOnChangeMenuHandler);
   }
 }
